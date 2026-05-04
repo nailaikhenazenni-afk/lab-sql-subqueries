cc15f5a95c9a27df50c3b45fff47427ee3d24c10
nailaikhenazenni-afk <naila.ikhenazen.ni@gmail.com>
2026-05-04 08:16:33 +0200

    Create resolved_lab_subqueries.sql

---
+++ resolved_lab_subqueries.sql
@@ -0,0 +1,50 @@
+USE sakila;
+
+SELECT 
+    f.title, 
+    COUNT(i.inventory_id) AS number_of_copies
+FROM 
+film f
+JOIN 
+    sakila.inventory i ON f.film_id = i.film_id
+WHERE 
+    f.title = 'HUNCHBACK IMPOSSIBLE'
+GROUP BY 
+    f.title;
+    
+SELECT 
+    title, 
+    length
+FROM 
+ film
+WHERE 
+    length > (SELECT AVG(length) FROM film)
+ORDER BY 
+    length DESC;
+
+SELECT 
+    first_name, 
+    last_name
+FROM 
+   actor
+WHERE 
+    actor_id IN (
+        SELECT 
+            actor_id
+        FROM 
+          film_actor
+        WHERE 
+            film_id = (
+                SELECT 
+                    film_id
+                FROM 
+                  film
+                WHERE 
+                    title = 'ALONE TRIP'
+            )
+    );
+ 
+    
+
+ 
+ 
